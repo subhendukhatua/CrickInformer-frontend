e16417c4cfbf21d475f2e075eb80c902a7c39920
subhendukhatua <subhendukhatua143@gmail.com>
2023-12-13 15:08:37 +0530

    fronend upload only

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { LiveComponent } from './pages/live/live.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component:HomeComponent,
+        title:"Home | CrickInfo"
+    },
+    {
+        path:"history",
+        component:HistoryComponent,
+        title:"Match History | CrickInfo"
+    },
+    {
+        path:"live",
+        component:LiveComponent,
+        title:"Live Matches | CrickInfo"
+    },
+    {
+        path:"point-table",
+        component:PointTableComponent,
+        title:"Point-Table | CrickInfo"
+    }
+];
